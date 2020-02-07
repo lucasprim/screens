71d896b651b9a7ceef973787c4d94074cc4b016c
Lucas Prim <lucas@zygotecnologia.com>
2020-02-07 18:18:34 -0300

    Added a new feature (load external URLs) and fixed a couple of bugs

--- screens.rb
+++ screens.rb
@@ -7,7 +7,7 @@ URL = "https://s3.amazonaws.com/zygo.screens/"
 EXECUTABLE = if Gem::Platform.local.os == "darwin"
 	"/Applications/Firefox.app/Contents/MacOS/firefox"
 else
-	"C:/Program Files/Mozilla Firefox/firefox.exe"
+	"C:\\Program Files\\Mozilla Firefox\\firefox.exe"
 end
 
 xml = URI.open(URL).read
@@ -17,10 +17,19 @@ files = []
 
 doc.root.each_element('//Contents') { |e| files.push(e.elements['Key'].text) }
 
-puts files
+extra_urls_present = false
+urls = files.map do |f|
+	extra_urls_present = true and next if f == 'urls.txt'
+	"\"#{URL}#{f}\""
+end.compact
 
-files.each do |f|
-	puts f
-	Kernel.system("#{EXECUTABLE} -kiosk -new-tab #{URL}#{f}")
+if extra_urls_present
+	extra_urls = URI.open("#{URL}urls.txt").read.split("\n")
+	extra_urls.each { |eu| urls.push(eu) }
+end
+
+urls.each do |u|
+	puts u
+	Kernel.system("\"#{EXECUTABLE}\" -kiosk -new-tab #{u}")
 	sleep 1
 end
